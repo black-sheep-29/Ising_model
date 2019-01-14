936b46b5bf80953d1dfce02d6f24e3313a8f833d
= <=>
2019-01-14 16:53:04 -0500

    Ajout des quelques fonctions d'initialisations

---
+++ Sources/chaine.jl
@@ -0,0 +1,53 @@
+using LinearAlgebra
+using Random
+
+struct Chaine
+    spins::Vector{Int}
+    couplages::Matrix{Float64}
+
+    function Chaine(spins, couplages)
+        if size(couplages) != (length(spins), length(spins))
+            error("La matrice de couplage doit avoir la dimension du nombre de spins.")
+        end
+        if sum(spins .== 0) + sum(spins .== 1) != length(spins)
+            error("Les spins doivent être 0 ou 1.")
+        end
+        if couplages != transpose(couplages)
+            error("La matrice doit être symétrique.")
+        end
+        return new(spins, couplages)
+    end
+end
+
+function systemeUnVoisin(spins::Vector{Int}, J::Float64)
+    n_spins = length(spins)
+    couplages = zeros(n_spins, n_spins)
+    for i in 1:(n_spins - 1)
+        couplages[i, i+1] = J
+        couplages[i+1, i] = J
+    end
+    return Chaine(spins, couplages)
+end
+
+function systemDeuxVoisins(spins::Vector{Int})
+    # ... À faire si besoin
+end
+
+function systemeAleatoire(spins::Vector{Int}, J_min::Float64, J_max::Float64)
+    n_spins = length(spins)
+    couplages = zeros(n_spins, n_spins)
+    for i in 1:(n_spins - 1)
+        for j in i+1:n_spins
+            x = J_min + rand() * (J_max - J_min)
+            couplages[i, j] = x
+            couplages[j, i] = x
+        end
+    end
+    return Chaine(spins, couplages)
+end
+
+function systemConstant(spins::Vector{Int}, J::Float64)
+    n_spins = length(spins)
+    couplages = J * (ones(n_spins, n_spins) - Matrix(I, n_spins, n_spins))
+    return Chaine(spins, couplages)
+end
