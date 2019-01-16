b3c0824ea23f846777dbcb782e52c78f063afb57
black-sheep-29 <jphoule29@hotmail.com>
2019-01-15 19:10:56 -0500

    energie

--- Sources/chaine.jl
+++ Sources/chaine.jl
@@ -28,25 +28,25 @@ length(chaine::Chaine) = length(chaine.spins)
 
 # Constructeurs
 
-function systemeUnVoisin(spins::Vector{Int}, J::Float64)        # EX: systemeUnVoisin([1, 1, 1], 1.0)
-    n_spins = length(spins)                                     #
-    couplages = zeros(n_spins, n_spins)                         #  0 0 0       0.0  1.0  0.0
-    for i in 1:(n_spins - 1)                                    #  0 0 0  -->  1.0  0.0  1.0
-        couplages[i, i + 1] = J                                 #  0 0 0       0.0  1.0  0.0
+function systemeUnVoisin(spins::Vector{Int}, J::Float64)  # EX: systemeUnVoisin([1, 1, 1], 1.0)
+    n_spins = length(spins)                               #
+    couplages = zeros(n_spins, n_spins)                   #  0 0 0       0.0  1.0  0.0
+    for i in 1:(n_spins - 1)                              #  0 0 0  -->  1.0  0.0  1.0
+        couplages[i, i + 1] = J                           #  0 0 0       0.0  1.0  0.0
         couplages[i + 1, i] = J
     end
     return Chaine(spins, couplages)
 end
 
-function systemeDeuxVoisins(spins::Vector{Int}, J_1::Float64, J_2::Float64)      # Ex: systemDeuxVoisins([0, 0, 0], 1.0)
-    n_spins = length(spins)                                                     #
-    couplages = zeros(n_spins, n_spins)                                         #  0 0 0       0.0  1.0  0.0                0.0  1.0  1.0
-    for i in 1:(n_spins - 1)                                                    #  0 0 0 -->   1.0  0.0  0.0 --> (Maxime)   1.0  0.0  1.0
-        couplages[i, i + 1] = J_1                                                 #  0 0 0       0.0  0.0  0.0                1.0  1.0  0.0
+function systemeDeuxVoisins(spins::Vector{Int}, J_1::Float64, J_2::Float64)  # Ex: systemDeuxVoisins([0, 0, 0], 1.0)
+    n_spins = length(spins)                                                  #
+    couplages = zeros(n_spins, n_spins)                                      #  0 0 0       0.0  1.0  1.0
+    for i in 1:(n_spins - 1)                                                 #  0 0 0  -->  1.0  0.0  1.0
+        couplages[i, i + 1] = J_1                                            #  0 0 0       1.0  1.0  0.0
         couplages[i + 1, i] = J_1
     end
 
-    # Maxime : Tu avais oublié le couplage avec le second voisin.
+    # couplage avec le second voisin.
     for i in 1:(n_spins - 2)
         couplages[i, i + 2] = J_2
         couplages[i + 2, i] = J_2
@@ -78,33 +78,24 @@ end
 
 # Maxime : systemeUnVoisin n'est pas un type, mais le nom d'une fonction. Le type est Chaine. De plus, comme tu as fais,
 # c'est simplement le nom de la variable. Va voir plus bas pour ma suggestion
-function calculer_energie(systemeUnVoisin)
-    energie = 0.0
-    for i in 1:length(couplages)
-        total = 0
-        for x in 1:(length(chaine) - 1)
-            if chaine.spins[x] == chaine.spins[x + 1]
-                total += 1
-            else
-                total -= 1
-            end
-        end
-        energie -= chaine.couplages[i] * total
-    end
-    return energie
-end
-
-function calculer_energie_maxime(chaine::Chaine)
+function calculer_energie(chaine::Chaine)
     energie = 0.0
     for i in 1:length(chaine)
-        for j in i+1:length(chaine)
+        alignement_des_spins = 0
+        for j in (i + 1):length(chaine)
             if chaine.spins[i] == chaine.spins[j]
-                alignement_des_spins = +1
+                alignement_des_spins += 1
             else
-                alignement_des_spins = -1
+                alignement_des_spins -= 1
             end
             energie -= chaine.couplages[i,j] * alignement_des_spins
         end
     end
     return energie
 end
+
+
+
+
+chaine = systemeUnVoisin([1, 1, 1], 1.0)
+println(calculer_energie(chaine))
