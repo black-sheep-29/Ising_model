f68098382a50e1e9c9d2b7f239f8344ea90ec14a
black-sheep-29 <jphoule29@hotmail.com>
2019-01-19 07:00:01 -0500

    Finaliser Tests

--- Tests/chaine.jl
+++ Tests/chaine.jl
@@ -4,10 +4,19 @@ using Test
 using Random
 
 # Tests Constructeurs
+
 @testset "Constructeurs" begin
-    # Max : Il y avait une paranthèse de trop à la ligne 9.
+
     @test systemeUnVoisin([1, 1, 1], 1.0) == Chaine([1, 1, 1], [0.0 1.0 0.0; 1.0 0.0 1.0; 0.0 1.0 0.0])
 
+    @test systemeDeuxVoisins([1, 1, 1], 1.0, 2.0) == Chaine([1, 1, 1], [0.0 1.0 2.0; 1.0 0.0 1.0; 2.0 1.0 0.0])
+
+    Random.seed!(42)
+    @test systemeAleatoire([1, 1, 1], 2.5, 10.0) == Chaine([1, 1, 1], [0.0 6.49887 5.90522; 6.49887 0.0 2.63265; 5.90522 2.63265 0.0])
+
+    @test systemeAleatoire([1, 1, 1], 3.0, 3.0) == Chaine([1, 1, 1], [0.0 3.0 3.0; 3.0 0.0 3.0; 3.0 3.0 0.0])
+
+    @test systemeConstant([1, 1, 1], 2.0) == Chaine([1, 1, 1], [0.0 2.0 2.0; 2.0 0.0 2.0; 2.0 2.0 0.0])
 end
 
 # Tests Énergie
@@ -17,16 +26,17 @@ end
     chaine = systemeUnVoisin([1, 1, 1, 0, 0], 3.0)
     @test calculer_energie(chaine) ≈ -6.0
 
-   chaine = systemeDeuxVoisins([1, 1, 1], 1.0, 1.0)     # erreur #
+   chaine = systemeDeuxVoisins([1, 1, 1], 1.0, 1.0)
    @test calculer_energie(chaine) ≈ -3.0
 
    chaine = systemeAleatoire([1, 1, 1], 1.0, 1.0)
    @test calculer_energie(chaine) ≈ -3.0
 
-   chaine = systemeConstant([1, 0, 1], -2.0)
-   @test calculer_energie(chaine) ≈ -2.0
-
    Random.seed!(42)
    chaine = systemeAleatoire([1, 1, 1], 3.0, 7.0)
    @test calculer_energie(chaine) ≈ -13.019595913383872
+
+   chaine = systemeConstant([1, 0, 1], -2.0)
+   @test calculer_energie(chaine) ≈ -2.0
+
 end
