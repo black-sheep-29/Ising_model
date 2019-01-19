cbfc39a00f6550b3ffbf7d6c154dafaca13f2ce2
black-sheep-29 <jphoule29@hotmail.com>
2019-01-19 07:55:09 -0500

    Création nouvelles fonctions

--- Sources/metropolis.jl
+++ Sources/metropolis.jl
@@ -20,24 +20,35 @@ function inverser_spin(chaine::Chaine, a)
     return chaine.spins
 end
 
-function energie(chaine::Chaine, a)
+function energie(chaine::Chaine, a)                 # vérifier erreur #
     alignement_des_spins = 0
-    if a != 1
-        if chaine.spins[a] == chaine.spins[a - 1]
-            alignement_des_spins += 1
-        else
-            alignement_des_spins -= 1
+    for i in 1:length(chaine)
+        for j in (i + 1):length(chaine)
+            if i != 1
+                if chaine.spins[i] == chaine.spins[j]
+                    alignement_des_spins += 1
+                else
+                    alignement_des_spins -= 1
+                end
+            end
+            if i != length(chaine)
+                if chaine.spins[i] == chaine.spins[j]
+                    alignement_des_spins += 1
+                else
+                    alignement_des_spins -= 1
+                end
+            end
         end
     end
-    if a != length(chaine)
-        if chaine.spins[a] == chaine.spins[a + 1]
-            alignement_des_spins += 1
-        else                                             # J-P:
-            alignement_des_spins -= 1                    # comment adapter le couplage pour une matrice ?
-        end
-    end
-    return -chaine.couplages[1] * alignement_des_spins
+    return -chaine.couplages[i, j] * alignement_des_spins
 end
 
-chaine = systemeUnVoisin([1, 1, 1, 0, 0], 3.0)
-println(energie(chaine, 2))
+function difference_energie(chaine::Chaine, a)                          # J-P: Pour tester la fonction il faut que
+    return energie(inverser_spin(chaine, a), a) - energie(chaine, a)    # que la fonction énergie fonctionne!
+end
+
+function magnetisation(chaine::Chaine)
+    nbr_haut = sum(chaine.spins)
+    nbr_bas = length(chaine) - nbr_haut
+    return (nbr_haut - nbr_bas)
+end
