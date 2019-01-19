68288a16a470c747053e3eeda2bf6fe14ca6ada0
black-sheep-29 <jphoule29@hotmail.com>
2019-01-19 17:06:31 -0500

    Création fonction Métropolis

--- Sources/metropolis.jl
+++ Sources/metropolis.jl
@@ -52,3 +52,48 @@ function magnetisation(chaine::Chaine)
     nbr_bas = length(chaine) - nbr_haut
     return (nbr_haut - nbr_bas)
 end
+
+function metropolis(chaine::Chaine, temperature::Float64, n_iters::Int, tolerence::Float64)
+    energie_systeme = [calculer_energie(chaine)]
+    magnetisation_systeme = [magnetisation(chaine)]
+
+    for i in 1:n_iters
+        c = coordonnee(chaine)
+        diff_E = difference_energie(chaine, c...)
+        if diff_E <= 0
+            chaine = inverser_spin(chaine, c...)
+        else
+            if rand() < exp(-diff_E/(k * temperature))
+                chaine = inverser_spin(chaine, c...)
+            end
+        end
+    end
+
+    push!(energie_systeme, calculer_energie(chaine))
+    push!(magnetisation_systeme, magnetisation(chaine))
+
+    j = 2
+    while abs(energie_systeme[j] - energie_systeme[j - 1]) > tolerence && j < 10000
+        for i in 1:n_iters
+            c = coordonnee(chaine)
+              diff_E = difference_energie(chaine, c...)
+            if diff_E <= 0
+                chaine = inverser_spin(chaine, c...)
+            else
+                if rand() < exp(-diff_E/(k * temperature))
+                    chaine = inverser_spin(chaine, c...)
+                end
+            end
+        end
+
+        push!(energie_systeme, calculer_energie(chaine))
+        push!(magnetisation_systeme, magnetisation(chaine))
+        j += 1
+    end
+    if j == 10000
+        println("Il n'y a pas de convergence")
+    end
+
+    println("Magnétisation moyenne:", (mean(magnetisation_systeme)))
+    return energie_systeme, magnetisation_systeme
+end
