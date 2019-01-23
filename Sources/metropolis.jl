590b754e475740cf6afd5af8cfbc35bec07c9ffe
= <=>
2019-01-23 09:58:47 -0500

    Déplacement de quelques fonctions et nouveaux tests

--- Sources/metropolis.jl
+++ Sources/metropolis.jl
@@ -13,46 +13,6 @@ function coordonnee(chaine::Chaine)
     return a
 end
 
-function inverser_spin(chaine::Chaine, a)
-    x = chaine.spins[a...]
-    x = 1 - x
-    chaine.spins[a...] = x
-    return chaine.spins
-end
-
-function energie(chaine::Chaine, a)                 # vérifier erreur #
-    alignement_des_spins = 0
-    for i in 1:length(chaine)
-        for j in (i + 1):length(chaine)
-            if i != 1
-                if chaine.spins[i] == chaine.spins[j]
-                    alignement_des_spins += 1
-                else
-                    alignement_des_spins -= 1
-                end
-            end
-            if i != length(chaine)
-                if chaine.spins[i] == chaine.spins[j]
-                    alignement_des_spins += 1
-                else
-                    alignement_des_spins -= 1
-                end
-            end
-        end
-    end
-    return -chaine.couplages[i, j] * alignement_des_spins
-end
-
-function difference_energie(chaine::Chaine, a)                          # J-P: Pour tester la fonction il faut que
-    return energie(inverser_spin(chaine, a), a) - energie(chaine, a)    # que la fonction énergie fonctionne!
-end
-
-function magnetisation(chaine::Chaine)
-    nbr_haut = sum(chaine.spins)
-    nbr_bas = length(chaine) - nbr_haut
-    return (nbr_haut - nbr_bas)
-end
-
 function metropolis(chaine::Chaine, temperature::Float64, n_iters::Int, tolerence::Float64)
     energie_systeme = [calculer_energie(chaine)]
     magnetisation_systeme = [magnetisation(chaine)]
--- Tests/metropolis.jl
+++ Tests/metropolis.jl
@@ -24,47 +24,11 @@ using Statistics
 
 end
 
-@testset "Inverser Spin" begin
-
-    chaine = systemeUnVoisin([0, 0, 0, 1], -1.0)
-    @test inverser_spin(chaine, 4) == [0, 0, 0 ,0]
-
-    chaine = systemeDeuxVoisins([1, 0, 0, 1, 1], 3.54, -2.0)
-    @test inverser_spin(chaine, 2) == [1, 1, 0, 1, 1]
-
-    chaine = systemeAleatoire([1, 1, 0, 0, 0], 2.0, 3.0)
-    @test inverser_spin(chaine, 1) == [0, 1, 0, 0, 0]
-
-    chaine = systemeConstant([0, 1, 1, 0], 3.45)
-    @test inverser_spin(chaine, 3) == [0, 1, 0, 0]
-end
 
 
-@testset "Énergie" begin
-
-end
 
 
-@testset "Différence d'Énergie" begin
 
-end
-
-
-@testset "Magnetisation" begin
-
-    chaine = systemeUnVoisin([0, 0, 0, 1], 2.5)
-    @test magnetisation(chaine) == -2
-
-    chaine = systemeDeuxVoisins([1, 1, 0], -1.0, 2.45)
-    @test magnetisation(chaine) == 1
-
-    chaine = systemeAleatoire([0, 0, 1, 0], 1.0, 1.0)
-    @test magnetisation(chaine) == -2
-
-    chaine = systemeConstant([1, 1, 1, 1, 1], -23.9)
-    @test magnetisation(chaine) == 5
-
-end
 
 @testset "Metropolis" begin
 
