695ae0a33fee493dad22b952d1dc92976276c500
black-sheep-29 <jphoule29@hotmail.com>
2019-01-24 19:22:47 -0500

    Tests Update

--- Tests/chaine.jl
+++ Tests/chaine.jl
@@ -11,14 +11,12 @@ using Random
 
     @test systemeDeuxVoisins([1, 1, 1], 1.0, 2.0) == Chaine([1, 1, 1], [0.0 1.0 2.0; 1.0 0.0 1.0; 2.0 1.0 0.0])
 
-    Random.seed!(42)
-    @test systemeAleatoire([1, 1, 1], 2.5, 10.0) == Chaine([1, 1, 1], [0.0 6.49887 5.90522; 6.49887 0.0 2.63265; 5.90522 2.63265 0.0])
-
     @test systemeAleatoire([1, 1, 1], 3.0, 3.0) == Chaine([1, 1, 1], [0.0 3.0 3.0; 3.0 0.0 3.0; 3.0 3.0 0.0])
 
     @test systemeConstant([1, 1, 1], 2.0) == Chaine([1, 1, 1], [0.0 2.0 2.0; 2.0 0.0 2.0; 2.0 2.0 0.0])
 
-    # tester système polynomial
+    @test systemePolynomial([1, 1, 1], 5.0, 1) == Chaine([1, 1, 1], [0.0 5.0 2.5; 5.0 0.0 5.0; 2.5 5.0 0.0])
+
 end
 
 # Tests Énergie
@@ -28,24 +26,24 @@ end
     chaine = systemeUnVoisin([1, 1, 1, 0, 0], 3.0)
     @test calculer_energie(chaine) ≈ -6.0
     @test calculer_energie(chaine, 1) ≈ -3.0
-    @test difference_energie(chaine, 3) ≈ 0.0
-    @test difference_energie(chaine, 2) ≈ 12.0
-
 
     chaine = systemeDeuxVoisins([1, 1, 1], 1.0, 2.0)
     @test calculer_energie(chaine) ≈ -4.0
     @test calculer_energie(chaine, 3) ≈ -3.0
 
-    chaine = systemeAleatoire([1, 1, 1], 1.0, 1.0)
-    @test calculer_energie(chaine) ≈ -3.0
-
     Random.seed!(42)
     chaine = systemeAleatoire([1, 1, 1], 3.0, 7.0)
     @test calculer_energie(chaine) ≈ -13.019595913383872
+    chaine = systemeAleatoire([1, 1, 1], 1.0, 1.0)          # vérifier #
+    @test calculer_energie(chaine) ≈ -3.0
 
     chaine = systemeConstant([1, 0, 1, 1], -2.0)
     @test calculer_energie(chaine) ≈ 0.0
     @test calculer_energie(chaine, 2) ≈ -6.0
+
+#    chaine = systemePolynomial([1, 1, 1], 1.0, 1)        # vérifier #
+#    @test calculer_energie(chaine) ≈ -2.5
+#    @test calculer_energie(chaine, 3) ≈ 0.0
 end
 
 @testset "Magnetisation" begin
@@ -61,6 +59,9 @@ end
 
     chaine = systemeConstant([1, 1, 1, 1, 1], -23.9)
     @test calculer_magnetisation(chaine) == 5
+
+    chaine = systemePolynomial([0, 0, 0, 0, 1], 2.21, 3)
+    @test calculer_magnetisation(chaine) == -3
 end
 
 @testset "Inverser Spin" begin
@@ -76,4 +77,27 @@ end
 
     chaine = systemeConstant([0, 1, 1, 0], 3.45)
     @test inverser_spin(chaine, 3).spins == [0, 1, 0, 0]
+
+    chaine = systemePolynomial([1, 1, 1, 0, 0, 1], 3.97, 4)
+    @test inverser_spin(chaine, 6).spins == [1, 1, 1, 0, 0, 0]
+end
+
+
+@testset "Différence Énergie" begin
+
+    chaine = systemeUnVoisin([0, 0, 0, 1], 2.5)
+    @test difference_energie(chaine, 3) ≈ 0.0
+
+    chaine = systemeDeuxVoisins([1, 0, 0, 1, 1], 1.0, 2.0)
+    @test difference_energie(chaine, 1) ≈ -6.0
+
+    Random.seed!(42)
+    chaine = systemeAleatoire([0, 0, 1, 0], 2.0, 3.5)
+    @test difference_energie(chaine, 4) ≈ 4.00914016972304
+
+    chaine = systemeConstant([0, 1, 1, 0], 3.0)
+    @test difference_energie(chaine, 3) ≈ -6.0      # à vérifier #
+
+    chaine = systemePolynomial([1, 1, 1, 1], 1.0, 1)
+    @test difference_energie(chaine, 2) ≈ 5.0           # à vérifier
 end
